6eed1a24fea504010e72dd4ca72ab5f88033c346
Learning <88524372+shimunana@users.noreply.github.com>
2022-10-15 00:00:21 +0800

    Update cut_r_web_wsn.py

--- cut_r_web_wsn.py
+++ cut_r_web_wsn.py
@@ -56,7 +56,7 @@ HBV_DNA_10000 =map[HBV_DNA_10000]
 # LOE =map[LOE]
 # gender = map[gender]
 # 数据读取，特征标注
-hp_train = pd.read_csv('E:\\Spyder_2022.3.29\\data\\machinel\\lrq_data\\liver_cut_data_recurrence.csv')
+hp_train = pd.read_csv('liver_cut_data_recurrence.csv')
 hp_train['Recurrence'] = hp_train['Recurrence'].apply(lambda x : +1 if x==1 else 0)
 features =["Gender","Tumor_size_3","Tumor_single","BCLC","HBV_DNA_10000",'N',"M",'NLR','Fibrinogen']
 target = 'Recurrence'
